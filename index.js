8b8efac778c0aa7d4f536b70c219f8986b8e704d
DidasMba <daniel.mbarushi@gmail.com>
2023-09-14 10:49:35 +0300

    portfolio init

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const name = "Joe";
 const height = 74;
+const voice = "slowly"
+const age = 576;
+
 const message = `${name} is ${height} inches tall`;
+const valeure = `${voice} is ${age}`;
 
 module.exports = { name, height, message };
 
 console.log(message);
+
+console.log(valeure);
+
 
 
 
--- test.js
+++ test.js
@@ -1,4 +1,18 @@
 
-a = 5;
-console.log(a);
+const lineOne = "Joy to the world";
+const lineTwo = "All the boys and girls";
+const lineThree = "Joy to the fishes in the deep blue sea";
+const lineFour = "Joy to you and me";
+
+const chorus = `${lineOne}\n${lineTwo}`;
+
+chorus;
+
+let i = 1;
+while (){
+
+}
+
+
+
 
